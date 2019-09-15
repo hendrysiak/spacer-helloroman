ac1eb3ceeeb70569ea7ae6667a521363fb91e035
hendrysiak <lukasz.hendrysiak@gmail.com>
2019-09-15 20:37:41 +0200

    Add basic view

--- src/router.js
+++ src/router.js
@@ -1,21 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Search from './views/Search.vue';
-import About from './views/About.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [{
-      path: '/',
-      name: 'Search',
-      component: Search,
-    },
-    {
-      path: '/about',
-      name: 'About',
+    routes: [{
+            path: '/',
+            name: 'Search',
+            component: Search,
+        },
 
-      component: About,
-    },
-  ],
+    ],
 });
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input id="search" name="search" type="text" v-model="searchValue" @input="handleInput" />
-      <ul>
-        <li v-for="item in results" key="item.data[0].nasa_id">
-          <p>{{item.data[0].description}}</p>
-        </li>
-      </ul>
-    </div>
+    <Claim />
+    <SearchInput />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import debounce from "lodash.debounce";
+import Claim from "@/components/Claim";
+import SearchInput from "@/components/SearchInput";
 const API = "https://images-api.nasa.gov/";
 
 export default {
   name: "Search",
+  components: { Claim, SearchInput },
   data() {
     return {
       searchValue: "",
@@ -42,27 +38,17 @@ export default {
 
 <style lang="scss" scoped>
 .wrapper {
+  margin: 0;
+  padding: 30px;
+  height: 100vh;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 0;
-  padding: 30px;
-  width: 100%;
-}
-
-.search {
-  display: flex;
-  flex-direction: column;
-  width: 300px;
-
-  label {
-    font-family: Montserrat, sans-serif;
-  }
-
-  input {
-    height: 30px;
-    border: 0;
-    border-bottom: 1px solid black;
-  }
+  justify-content: center;
+  background-image: url("../assets/heroimage.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 80% 0%;
 }
 </style>
